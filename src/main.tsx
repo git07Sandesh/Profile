c32c81117f551d48136cb839f8d3e08e42c873ea
Sandesh Bhattarai <sandesh1122bhattarai@gmail.com>
2025-05-06 14:11:41 -0500

    Modified dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+const savedTheme = localStorage.getItem('theme');
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+if (savedTheme === 'dark' || (!savedTheme && prefersDark)) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
